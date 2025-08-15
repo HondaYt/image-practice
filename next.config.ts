82703d928a28e61a2937f5834e7f612ebc621554
HondaYt <honda.yuto@sagj.co.jp>
2025-08-15 17:22:38 +0900

    Imageの挙動調査

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
 };
+
+module.exports = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [new URL('https://chatgpt.com/**')],
+  },
+}
 
 export default nextConfig;
