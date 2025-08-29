12cfb8af01f4e0c056ec09f1a8a4b612470c5f23
HondaYt <honda.yuto@sagj.co.jp>
2025-08-29 18:58:53 +0900

    decoding

---
+++ src/app/decoding-async/page.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from '@/app/page.module.css';
+
+export default function Home() {
+    return (
+        <>
+            <p>↓CUTE↓</p>
+            <img
+                className={styles.image}
+                src={'/sample.png'}
+                alt="かわいい猫"
+                decoding='async'
+            />
+            <p>↑CUTE↑</p>
+        </>
+    );
+}
