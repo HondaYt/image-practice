f4c6ac60fab9b0136d6d90586e84be2aa4bc7bd9
HondaYt <honda.yuto.04@gmail.com>
2025-09-01 04:32:58 +0900

    props

---
+++ src/app/getImageProps2/page.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @next/next/no-img-element, jsx-a11y/alt-text */
+import styles from '@/app/page.module.css';
+import sample from "@/assets/sample.png";
+import { getImageProps } from "next/image";
+
+const props = getImageProps({
+    src: sample,
+    alt: "かわいい猫",
+    priority: true
+})
+
+export default function Home() {
+    return (
+        <img {...props.props} className={styles.image} />
+    );
+}
---
+++ src/app/unoptimized/page.tsx
@@ -0,0 +1,15 @@
+import styles from '@/app/page.module.css';
+import sample from "@/assets/sample.png";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <Image
+            className={styles.image}
+            src={sample}
+            alt="かわいい猫"
+            unoptimized
+            placeholder='blur'
+        />
+    );
+}
