6889854e052f9935b9954b852072cc500947307d
HondaYt <honda.yuto.04@gmail.com>
2025-10-06 06:29:17 +0900

    priority

---
+++ src/app/studyMeeting/priority/1/page.tsx
@@ -0,0 +1,17 @@
+import sample from "@/assets/sample.png";
+import Image from "next/image";
+import styles from './page.module.css';
+
+export default function Page() {
+    return (
+        <Image
+            className={styles.image}
+            src={sample}
+            alt='かわいい猫'
+            sizes="(max-width: 768px) 100vw, 400px"
+            quality={50}
+            priority
+            fetchPriority="high"
+        />
+    );
+}
